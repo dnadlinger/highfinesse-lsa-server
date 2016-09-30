d1cf62deed70bd5d1ecbc9871a5b8badb599a5ac
David Nadlinger <code@klickverbot.at>
2016-09-30 12:34:39 +0100

    Artiq RPC interface, sample time chunking implemented

--- highfinesse_lsa/server.py
+++ highfinesse_lsa/server.py
@@ -1,11 +1,69 @@
-from . import wlm_data
+#!/usr/bin/env python3
+
+import argparse
+import asyncio
+import numpy as np
+
+from .sample_chunker import SampleChunker
+from .wlm_data import LSA, MeasurementType
+from typing import Iterable
+from artiq.protocols.pc_rpc import simple_server_loop
+from artiq.tools import verbosity_args, simple_network_args, init_logger
+
+
+class RPCInterface:
+    def __init__(self, lsa: LSA, channels: Iterable[SampleChunker]):
+        self._lsa = lsa
+        for c in channels:
+            self.__setattr__("get_latest_" + c.name, c.get_latest)
+            self.__setattr__("get_new_" + c.name, c.get_new)
+
+    def get_latest_spectrum(self):
+        """Read the current spectrum an"""
+        meas = self._lsa.get_analysis_trace()
+        return np.vstack([np.ctypeslib.as_array(x) for x in meas])
+
+
+def get_argparser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser()
+    simple_network_args(parser, 4008)
+    verbosity_args(parser)
+    return parser
+
+
+def main() -> None:
+    args = get_argparser().parse_args()
+    init_logger(args)
+
+    loop = asyncio.get_event_loop()
+
+    channels = dict()
+
+    def reg_chan(name: str, meas_type: MeasurementType) -> None:
+        # TODO: Make callback log into Grafana.
+        binner = SampleChunker(loop, name, lambda _: print("Finished " + name),
+                               256, 30)
+        channels[meas_type] = binner
+
+    reg_chan("temperature_celsius", MeasurementType.temperature)
+    reg_chan("air_pressure_mbar", MeasurementType.air_pressure)
+    reg_chan("wavelength_vac_nm", MeasurementType.wavelength)
+    reg_chan("linewidth_vac_nm", MeasurementType.linewidth)
+    reg_chan("exposure_1_ms", MeasurementType.exposure_time_1)
+    reg_chan("exposure_2_ms", MeasurementType.exposure_time_2)
+
+    def meas_cb(meas_type, meas_value):
+        if meas_type in channels:
+            channels[meas_type].push(float(meas_value))
+
+    lsa = LSA()
+
+    # Bridge from the driver thread to the main thread.
+    lsa.add_callback(lambda *a: loop.call_soon_threadsafe(lambda: meas_cb(*a)))
+
+    rpc_interface = RPCInterface(lsa, channels.values())
+    simple_server_loop({"lsa": rpc_interface}, args.bind, args.port)
+
 
 if __name__ == "__main__":
-    import logging
-    logging.basicConfig()
-    logging.getLogger().setLevel(logging.DEBUG)
-    l = wlm_data.LSA()
-    l.add_callback(lambda ty, val: print("{}: {}".format(ty, val)))
-    while True:
-        import time
-        time.sleep(1)
+    main()
--- highfinesse_lsa/wlm_data.py
+++ highfinesse_lsa/wlm_data.py
@@ -8,7 +8,7 @@ analyser.
 import logging
 
 from .wlm_data_constants import *
-from ctypes import CFUNCTYPE, c_double, c_long, c_ssize_t, windll
+from ctypes import CFUNCTYPE, c_double, c_long, c_ssize_t, POINTER, windll
 from enum import Enum, unique
 from typing import Callable, Union
 import threading
@@ -65,6 +65,43 @@ def _check_get_wlm_version_error(code: c_long):
     raise WlmDataException("Unknown error in GetWLMVersion.")
 
 
+def _check_set_error(code: c_long):
+    """Checks the return value of the Set* family of functions, throwing an
+    exception if it indicates an error."""
+
+    if code == ResERR_NoErr:
+        return
+
+    messages = {
+        ResERR_WlmMissing: "No matching LSA server instance active.",
+        ResERR_CouldNotSet: "Value could not be set due to an internal error.",
+        ResERR_ParmOutOfRange: "Value to be set exceeds the allowed range.",
+        ResERR_WlmOutOfResources: "LSA server out of memory or resources.",
+        ResERR_WlmInternalError: "LSA server internal error.",
+        ResERR_NotAvailable: "Parameter not available in this LSA version.",
+        ResERR_WlmBusy: "The LSA server was busy.",
+        ResERR_NotInMeasurementMode: "Function call not allowed in "
+                                     "measurement mode.",
+        ResERR_OnlyInMeasurementMode: "Function call only allowed in "
+                                      "measurement mode.",
+        ResERR_ChannelNotAvailable: "Channel index out of range.",
+        ResERR_ChannelTemporarilyNotAvailable: "The given channel temporarily isn't"
+                                               "available (not in switch mode?).",
+        ResERR_CalOptionNotAvailable: "Given calibration option not supported "
+                                      "by this LSA.",
+        ResERR_CalWavelengthOutOfRange: "Calibration wavelength outside the "
+                                        "allowed range.",
+        ResERR_BadCalibrationSignal: "Calibration signal is of bad quality "
+                                     "(does not match given wavelength?).",
+        ResERR_UnitNotAvailable: "Result unit not available."  # It's a mystery.
+    }
+
+    msg = messages.get(code, None)
+    if not msg:
+        msg = "Unknown error occurred (code: {}).".format(code)
+    raise WlmDataException(msg)
+
+
 class Driver:
     """Thin wrapper to annotate the driver DLL with types and return value checking."""
     def __init__(self):
@@ -95,6 +132,15 @@ class Driver:
             self.get_wlm_version.restype = lambda x: _check_get_wlm_version_error(x)
 
             self.get_channels_count = get_fn("GetChannelsCount", c_long, [c_long])
+
+            self.get_analysis_item_count = get_fn("GetAnalysisItemCount",
+                                                  c_long, [c_long])
+            self.get_analysis_item_size = get_fn("GetAnalysisItemSize",
+                                                 c_long, [c_long])
+            self.get_analysis_data = get_fn("GetAnalysisData",
+                                            c_long, [c_long, POINTER(c_double)])
+            self.set_analysis = get_fn("SetAnalysis", c_long, [c_long, c_long])
+            self.set_analysis.restype = lambda x: _check_set_error(x)
         except Exception as e:
             raise WlmDataException("Error binding to function from DLL: {}".format(e))
 
@@ -103,10 +149,10 @@ class Driver:
 class MeasurementType(Enum):
     #: Reading of the temperature sensor in the device's optics block, in
     #: degrees Celsius.
-    deviceTemperature = cmiTemperature
+    temperature = cmiTemperature
 
     #: Reading of the pressure sensor in the device's optics block, in mbar.
-    airPressure = cmiPressure
+    air_pressure = cmiPressure
 
     #: Vacuum wavelength, in nm.
     wavelength = cmiWavelength1
@@ -116,16 +162,17 @@ class MeasurementType(Enum):
 
     #: Exposure time 1, in milliseconds. Updated on manual changes as well as
     #: due to automatic adjustment being active.
-    exposureTime1 = cmiExposureValue1
+    exposure_time_1 = cmiExposureValue1
 
-    #: Exposure time 2, in milliseconds. Updated on manual changes as well as
-    #: due to automatic adjustment being active.
-    exposureTime2 = cmiExposureValue2
+    #: Exposure time 2, in milliseconds. (Added to exposure_time_1 to determine
+    #: the exposure for the fine grating CCD.) Updated on manual changes as well
+    #: as due to automatic adjustment being active.
+    exposure_time_2 = cmiExposureValue2
 
 
 _DOUBLE_MEASUREMENT_TYPES = {
-    MeasurementType.deviceTemperature,
-    MeasurementType.airPressure,
+    MeasurementType.temperature,
+    MeasurementType.air_pressure,
     MeasurementType.wavelength,
     MeasurementType.linewidth
 }
@@ -195,6 +242,10 @@ class LSA:
                                  self._c_callback,
                                  c_long(0))
 
+        # Make analysis data arrays available to this process (this is distinct
+        # from the analysis mode).
+        self._driver.set_analysis(cSignalAnalysis, 1)
+
     def __del__(self):
         # We need to make sure to unregister the callback before the associated
         # ctypes object (self._c_callback) is destructed. CPython's documentation
@@ -206,7 +257,8 @@ class LSA:
                                  c_long(cNotifyRemoveCallback),
                                  c_ssize_t(0), c_long(0))
 
-    def add_callback(self, cb: Callable[[MeasurementType, Union[int, float]], None]) -> None:
+    def add_callback(self, cb: Callable[[MeasurementType,
+                                         Union[int, float]], None]) -> None:
         """
         Register a callback to be invoked when a new measurement result arrives.
 
@@ -218,12 +270,41 @@ class LSA:
         with self._result_callbacks_lock:
             self._result_callbacks.append(cb)
 
-    def remove_callback(self, cb: Callable[[MeasurementType, Union[int, float]], None]) -> None:
+    def remove_callback(self, cb: Callable[[MeasurementType,
+                                            Union[int, float]], None]) -> None:
         """Unregister a previously added measurement result callback."""
         with self._result_callbacks_lock:
             self._result_callbacks.remove(cb)
 
-    def _callback_ex(self, ver: c_long, mode: c_long, intval: c_long, dblval: c_double, res1: c_long) -> None:
+    def get_analysis_trace(self):
+        """
+        Retrieves the latest analysis "pattern" (trace) from the server
+        application.
+
+        :return: A tuple (wavelengths, amplitudes) of two ctypes arrays of
+            c_doubles, representing the x- and y-axes of the LSA analysis graph
+            (amplitude per wavelength on the CCD).
+        """
+
+        length = self._driver.get_analysis_item_count(cSignalAnalysis)
+        if length < 0:
+            raise WlmDataException("Analysis trace data not ready.")
+
+        elem_size = self._driver.get_analysis_item_size(cSignalAnalysis)
+        if elem_size != 8:
+            raise WlmDataException("Unexpected data type in analysis data "
+                                   "(expected double, got size: {}).".format(elem_size))
+
+        wavelengths = (c_double * length)()
+        self._driver.get_analysis_data(cSignalAnalysisX, wavelengths)
+
+        amplitudes = (c_double * length)()
+        self._driver.get_analysis_data(cSignalAnalysisY, amplitudes)
+
+        return wavelengths, amplitudes
+
+    def _callback_ex(self, ver: c_long, mode: c_long, intval: c_long,
+                     dblval: c_double, res1: c_long) -> None:
         if ver != self._device_version:
             self._log.warn("Ignoring callback invocation for wrong device "
                            "version: %s (should be %s)",
